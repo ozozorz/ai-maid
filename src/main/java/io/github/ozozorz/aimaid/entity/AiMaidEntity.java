9e55423f1ee9a88b890e816f15fecd974eb5b6dd
zhoukuang233 <46531480+ozozorz@users.noreply.github.com>
2026-08-22 11:47:57 +0800

    一个非常小的 Brain

--- src/main/java/io/github/ozozorz/aimaid/entity/AiMaidEntity.java
+++ src/main/java/io/github/ozozorz/aimaid/entity/AiMaidEntity.java
@@ -1,14 +1,16 @@
 package io.github.ozozorz.aimaid.entity;
 
+import com.google.common.collect.ImmutableList;
+
+import io.github.ozozorz.aimaid.entity.ai.AiMaidAi;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.minecraft.world.level.Level;
 
 // 女仆实体类
@@ -28,16 +30,74 @@ public class AiMaidEntity extends PathfinderMob {
                 .add(Attributes.MOVEMENT_SPEED, 0.3);
     }
 
+    // Brain.Provider 不是 Brain。
+    // Brain.Provider = “大脑生产说明书”。
+    // Brain<AiMaidEntity> = 某一只具体 AiMaid 自己的大脑
+    // Provider 这两个参数分别是什么？
+    // 第一个：Sensor 列表。告诉 Provider：AiMaid 有哪些感知器。
+    // 第二个：AiMaidAi::getActivities 是方法引用。当前 26.2 的：Brain.ActivitySupplier<E>
+    // 是一个函数式接口：List<ActivityData<E>> createActivities(E body);
+    private static final Brain.Provider<AiMaidEntity> BARIN_PROVIDER = Brain.provider(
+            ImmutableList.of(SensorType.NEAREST_LIVING_ENTITIES, SensorType.NEAREST_PLAYERS), AiMaidAi::getActivities);
+
+    // rain.Packed = 从存档读取出来、准备恢复进 Brain 的记忆包。
     @Override
-    protected void registerGoals() {
-        // 掉进水里 → 尝试浮起来
-        // 没事干 → 到处走走
-        // 看到玩家 → 看玩家
-        // 其他时候 → 随机转头
-        this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new RandomStrollGoal(this, 1.0));
-        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 8.0f));
-        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+    protected Brain<AiMaidEntity> makeBrain(Brain.Packed packedBrain) {
+        return BARIN_PROVIDER.makeBrain(this, packedBrain);
     }
+
+    // 给 getBrain() 一个准确的泛型返回类型
+    @Override
+    @SuppressWarnings("unchecked")
+    public Brain<AiMaidEntity> getBrain() {
+        return (Brain<AiMaidEntity>) super.getBrain();
+    }
+
+    // 最重要的方法：customServerAiStep 让 Brain 真正“活起来”的地方。
+    @Override
+    protected void customServerAiStep(ServerLevel level) {
+        this.getBrain().tick(level, this);
+
+        AiMaidAi.updateActivity(this);
+
+        super.customServerAiStep(level);
+
+        /// DEBUG
+        if (this.tickCount % 40 == 0) {
+            Brain<AiMaidEntity> brain = this.getBrain();
+
+            System.out.println(
+                    "ACTIVE = "
+                            + brain.getActiveNonCoreActivity());
+
+            System.out.println(
+                    "NEAREST_PLAYERS = "
+                            + brain.getMemory(
+                                    MemoryModuleType.NEAREST_PLAYERS));
+
+            System.out.println(
+                    "LOOK_TARGET = "
+                            + brain.getMemory(
+                                    MemoryModuleType.LOOK_TARGET));
+
+            System.out.println(
+                    "WALK_TARGET = "
+                            + brain.getMemory(
+                                    MemoryModuleType.WALK_TARGET));
+        }
+        /// DUBUG END
+    }
+
+    // @Override
+    // protected void registerGoals() {
+    // // 掉进水里 → 尝试浮起来
+    // // 没事干 → 到处走走
+    // // 看到玩家 → 看玩家
+    // // 其他时候 → 随机转头
+    // this.goalSelector.addGoal(0, new FloatGoal(this));
+    // this.goalSelector.addGoal(1, new RandomStrollGoal(this, 1.0));
+    // this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 8.0f));
+    // this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+    // }
 
 }
---
+++ src/main/java/io/github/ozozorz/aimaid/entity/ai/AiMaidAi.java
@@ -0,0 +1,124 @@
+package io.github.ozozorz.aimaid.entity.ai;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+
+import io.github.ozozorz.aimaid.entity.AiMaidEntity;
+import net.minecraft.world.entity.EntityTypes;
+import net.minecraft.world.entity.ai.ActivityData;
+import net.minecraft.world.entity.ai.behavior.BehaviorControl;
+import net.minecraft.world.entity.ai.behavior.DoNothing;
+import net.minecraft.world.entity.ai.behavior.LookAtTargetSink;
+import net.minecraft.world.entity.ai.behavior.MoveToTargetSink;
+import net.minecraft.world.entity.ai.behavior.RandomStroll;
+import net.minecraft.world.entity.ai.behavior.RunOne;
+import net.minecraft.world.entity.ai.behavior.SetEntityLookTarget;
+import net.minecraft.world.entity.ai.behavior.Swim;
+import net.minecraft.world.entity.schedule.Activity;
+
+public class AiMaidAi {
+
+    private AiMaidAi() {
+    }
+
+    public static List<ActivityData<AiMaidEntity>> getActivities(AiMaidEntity maid) {
+        return List.of(initCoreActivity(), initIdleActivity());
+    }
+
+    private static ActivityData<AiMaidEntity> initCoreActivity() {
+        return ActivityData.create(Activity.CORE, 0, ImmutableList.<BehaviorControl<? super AiMaidEntity>>of(
+                new Swim<AiMaidEntity>(0.8F), new LookAtTargetSink(45, 90), new MoveToTargetSink()));
+    }
+
+    private static ActivityData<AiMaidEntity> initIdleActivity() {
+        return ActivityData.create(Activity.IDLE, 10, ImmutableList.of(createIdleBehaviors()));
+    }
+
+    private static RunOne<AiMaidEntity> createIdleBehaviors() {
+        return new RunOne<>(ImmutableList.of(Pair.of(SetEntityLookTarget.create(EntityTypes.PLAYER, 8.0F), 2),
+                Pair.of(RandomStroll.stroll(1.0F), 2), Pair.of(new DoNothing(30, 60), 1)));
+    }
+
+    public static void updateActivity(AiMaidEntity maid) {
+        maid.getBrain().setActiveActivityToFirstValid(List.of(Activity.IDLE));
+    }
+
+}
+
+// 可以把 Activity 理解成：
+// “我目前处在哪一种 AI 工作模式？”
+
+// 以后可能有：
+// CORE
+// IDLE
+// FOLLOW_OWNER
+// STAY
+// WORK
+// FIGHT
+// SLEEP
+
+// 但是 CORE 比较特殊。
+// 它更像：
+// 不管你现在处于什么模式，都需要存在的基础执行层。
+// 例如以后 AiMaid 在：
+// IDLE
+// 需要走路。
+// 在：
+// FOLLOW_OWNER
+// 也需要走路。
+// 在：
+// WORK
+// 还是需要走路。
+// 那么：
+// MoveToTargetSink
+// 就不应该分别复制到：
+// IDLE
+// FOLLOW_OWNER
+// WORK
+// 三个 Activity。
+// 而应该放：
+// CORE
+// 里。
+// 所以我们现在的设计是：
+// CORE:
+// LookAtTargetSink
+// MoveToTargetSink
+// Swim
+// ↑
+// │ 消费意图
+// │
+// IDLE
+// 看玩家 / 随机走 / 发呆
+// 这就是 Brain 非常漂亮的地方。
+
+// ActivityData 到底是什么？
+// 这一行：
+// ActivityData.create(
+// Activity.CORE,
+// 0,
+// ...
+// )
+// 不要把 ActivityData 理解成：
+// “正在运行的 Activity 对象”。
+// 它更像：
+// 这个 Activity 的配置说明书。
+// 26.2 当前的 ActivityData 里面包含 Activity 类型、Behavior 与优先级的配对、启动条件、退出时需要清除的 Memory
+// 等；它也提供我们现在使用的便捷 create(Activity, priority, behaviors) 方法。
+
+// 因此：
+// ActivityData.create(
+// Activity.CORE,
+// 0,
+// behaviors
+// )
+// 是在描述：
+// Activity：
+// CORE
+// 第一组 Behavior priority：
+// 0
+// 里面有哪些 Behavior：
+// Swim
+// LookAtTargetSink
+// MoveToTargetSin
